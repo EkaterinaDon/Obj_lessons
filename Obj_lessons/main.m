7f272cc7368753ddffc9cb577d90e75d2b85c37d
Ekaterina Donskaya <katrine.d@gmail.com>
2021-01-28 18:19:08 +0200

    Lesson_1

--- Obj_lessons/main.m
+++ Obj_lessons/main.m
@@ -9,8 +9,47 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //1. Создать калькулятор на основе практической задачи 3.
+        //2. Улучшить калькулятор благодаря выводу результата, а также переменных в консоль одной строкой (как в практической задаче 2)
+        
+        NSInteger firstNumber;
+        NSInteger secondNumber;
+        char operator;
+        
+        
+        printf("first number: ");
+        scanf("%ld", &firstNumber);
+        printf("operator +, -, *, / : ");
+        scanf("%s", &operator);
+        
+        printf("second number: ");
+        scanf("%ld", &secondNumber);
+        
+        switch (operator) {
+            case '+':
+                NSLog(@"%ld %c %ld = %ld", (long)firstNumber, operator, (long)secondNumber, firstNumber+secondNumber);
+                break;
+            case '-':
+                NSLog(@"%ld %c %ld = %ld", (long)firstNumber, operator, (long)secondNumber, firstNumber-secondNumber);
+                break;
+            case '*':
+                NSLog(@"%ld %c %ld = %ld", (long)firstNumber, operator, (long)secondNumber, firstNumber*secondNumber);
+                break;
+            case '/':
+                if (secondNumber == 0) {
+                    NSLog(@"Error! You can't divide by 0!");
+                    break;
+                } else {
+                    NSLog(@"%ld %c %ld = %ld", (long)firstNumber, operator, (long)secondNumber, firstNumber/secondNumber);
+                    break;
+                }
+        }
+        
+        //3. *Создать приложение, которое будет вычислять среднее число из трех переменных без применения специальных функций.
+        CGFloat a, b, c;
+        printf("input three numbers: ");
+        scanf("%lf, %lf, %lf", &a, &b, &c);
+        NSLog(@"Average of %lf, %lf, %lf = %lf", a, b, c, (a+b+c)/3);
     }
     return 0;
 }
