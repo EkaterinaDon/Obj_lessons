42d4ee5532611d143d3d3560754520203799fa9c
Ekaterina Donskaya <katrine.d@gmail.com>
2021-01-28 17:08:10 +0200

    Initial Commit

---
+++ Obj_lessons/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Obj_lessons
+//
+//  Created by Ekaterina on 28.01.21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
